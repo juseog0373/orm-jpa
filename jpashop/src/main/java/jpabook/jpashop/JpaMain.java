3a47d99546eac98f303882dfcfdaf6d24618e0c8
jskim <juseog0373@naver.com>
2024-07-09 17:39:07 +0900

    예제1 - 요구사항 분석, 필드매핑(2)

--- jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jpabook.jpashop.domain.Order;
 
 public class JpaMain {
     public static void main(String[] args) {
